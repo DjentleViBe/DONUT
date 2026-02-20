140d4cabf6028519350cf22a1dfc9478975b95eb
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2026-02-20 20:45:50 +0100

    config file for global configuration management

---
+++ config.py
@@ -0,0 +1,8 @@
+"""Configuration file for DONUT project.
+This file contains global configuration parameters and settings 
+that can be used across the project."""
+
+# plotting parameters
+color = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728",
+          "#9467bd", "#8c564b", "#e377c2", "#7f7f7f",
+            "#bcbd22", "#17becf"]
