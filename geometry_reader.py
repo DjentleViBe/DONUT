91919ed94c3303117202ce10e2f46bb270480e15
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2026-02-21 22:15:30 +0100

    read geometry

--- geometry_reader.py
+++ geometry_reader.py
@@ -5,27 +5,20 @@ poloidal section files. It uses the file operations to list the files
 in the input directory and read the JSON files to extract the necessary 
 parameters for building the sketches of the sectors.
 """
-
 import json
-from file_operations import list_files_in_directory
-from geometry_check import param_num_sector
+from geometry_check import param_num_sector, param_num_sector_toroidal
 
-def get_poloidal_sections_from_toroidal_file():
+def get_poloidal_sections_from_toroidal_file(filename):
     """Read the toroidal section file to get the list of poloidal sections to process.
     Returns:
     List of poloidal section file names (without extension) to process.
     """
-    list_of_files = list_files_in_directory("./inputs/")
-    for file in list_of_files:
-        if "toroidal_section" in file:
-            print("Found toroidal section file:", file)
-            with open(file, "r", encoding="utf-8") as f:
-                data = json.load(f)
-                # get the list of poloidal sections
-                poloidal_sections = data.get("main_sections", [])
-                print("Poloidal sections to process:", poloidal_sections)
-                return poloidal_sections
-    raise FileNotFoundError("No toroidal section file found in inputs directory.")
+    with open(filename, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        # get the list of poloidal sections
+        poloidal_sections = data.get("main_sections", [])
+        print("Poloidal sections to process:", poloidal_sections)
+        return poloidal_sections
 
 def get_geometry_parameters_from_poloidal_file(poloidal_file):
     """Read the poloidal section file to get the geometry parameters for building the sketch.
@@ -43,3 +36,23 @@ def get_geometry_parameters_from_poloidal_file(poloidal_file):
         degree = data.get("degree")
         param_num_sector(nval, theta, radius)
         return {"N_s": nval, "theta": theta, "radius": radius, "weights": weights, "degree": degree}
+
+def get_geometry_parameters_from_toroidal_file(toroidal_file):
+    """Read the toroidal section file to get the geometry parameters for building the sketch.
+    Args:
+        toroidal_file (str): The path to the toroidal section file.
+    Returns:
+        dict: A dictionary containing the geometry parameters.
+    """
+    with open(toroidal_file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        nval = data.get("N_t")
+        theta = data.get("theta")
+        phi = data.get("phi")
+        radius = data.get("radius")
+        weights = data.get("weights")
+        degree = data.get("degree")
+        param_num_sector_toroidal(nval, theta, phi, radius)
+        return {"N_t": nval, "theta": theta, "phi": phi,
+                "radius": radius, "weights": weights,
+                "degree": degree}
