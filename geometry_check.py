79b6098b22f2d83b5e734f72eabf16d6932c594b
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2026-02-21 22:15:01 +0100

    toroidal check

--- geometry_check.py
+++ geometry_check.py
@@ -16,3 +16,21 @@ def param_num_sector(ns, theta, radius):
     if len(radius) != ns:
         raise ValueError(f"Number of radius values should be {ns}")
     print("Geometry parameters are consistent.")
+
+def param_num_sector_toroidal(nt, theta, phi, radius):
+    """Check the geometry parameters for consistency for toroidal section.
+    Args:        N_t (int): Number of toroidal sectors.
+        theta (list): List of theta values for control points.
+        phi (list): List of phi values for control points.
+        radius (list): List of radius values for control points.
+    Raises:        ValueError: If the parameters are inconsistent.
+    """
+    print(f"Checking toroidal geometry parameters: N_t={nt},"
+          f"theta={theta}, phi={phi}, radius={radius}")
+    if len(theta) != nt:
+        raise ValueError(f"Number of theta values should be {nt}")
+    if len(phi) != nt:
+        raise ValueError(f"Number of phi values should be {nt}")
+    if len(radius) != nt:
+        raise ValueError(f"Number of radius values should be {nt}")
+    print("Toroidal geometry parameters are consistent.")
