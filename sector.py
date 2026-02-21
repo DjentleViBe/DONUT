26d4c39fa49c5845fa111a904dc8661ad5495145
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2026-02-21 22:15:42 +0100

    sector file

--- sector.py
+++ sector.py
@@ -5,7 +5,8 @@ read from the input files. It uses the geometry operations to compute the NURBS 
 and control points, and prepares the data for plotting.
 """
 from geometry_operations import nurbs_curve_periodic, \
-                                get_cartesian_coordinates, \
+                                get_cartesian_coordinates_2d, \
+                                get_cartesian_coordinates_3d, \
                                 generate_periodic_data
 
 def build_sketch_sector(theta, radius, degree, weights):
@@ -20,7 +21,7 @@ def build_sketch_sector(theta, radius, degree, weights):
     y: list of y coordinates of the curve points
     ctrl_x: list of x coordinates of the control points
     ctrl_y: list of y coordinates of the control points"""
-    ctrl_pts = get_cartesian_coordinates(theta, radius)
+    ctrl_pts = get_cartesian_coordinates_2d(theta, radius)
     ctrl_ext, knot, (u_start, u_end) = generate_periodic_data(
         ctrl_pts,
         degree
@@ -38,3 +39,35 @@ def build_sketch_sector(theta, radius, degree, weights):
     x, y = zip(*curve_points)
     ctrl_x, ctrl_y = zip(*ctrl_pts)
     return x, y, ctrl_x, ctrl_y
+
+def build_sketch_sector_toroidal(theta, phi, radius, degree, weights):
+    """Build the sketch of a toroidal sector based on the geometry parameters.
+    Args:   
+    theta: list of angles in degrees for the poloidal direction    
+    phi: list of angles in degrees for the toroidal direction
+    radius: list of corresponding radii
+    degree: degree of the NURBS curve
+    weights: list of weights for the control points
+    Returns:
+    x: list of x coordinates of the curve points
+    y: list of y coordinates of the curve points
+    ctrl_x: list of x coordinates of the control points
+    ctrl_y: list of y coordinates of the control points"""
+    ctrl_pts = get_cartesian_coordinates_3d(theta, phi, radius)
+    ctrl_ext, knot, (u_start, u_end) = generate_periodic_data(
+        ctrl_pts,
+        degree
+    )
+    weights_ext = weights + weights[:degree]
+    # Compute curve points
+    curve_points = nurbs_curve_periodic(
+        [ctrl_ext,
+        weights_ext,
+        degree,
+        knot,
+        u_start,
+        u_end]
+    )
+    x, y, z = zip(*curve_points)
+    ctrl_x, ctrl_y, ctrl_z = zip(*ctrl_pts)
+    return x, y, z, ctrl_x, ctrl_y, ctrl_z
