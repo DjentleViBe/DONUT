44362d396a661f9adde6489db731ada1d639327c
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2026-02-22 18:50:55 +0100

    main

--- main.py
+++ main.py
@@ -7,16 +7,31 @@ section of the geometry.
 from geometry_reader import get_poloidal_sections_from_toroidal_file, \
                             get_geometry_parameters_from_poloidal_file,\
                             get_geometry_parameters_from_toroidal_file
-from sector import build_sketch_sector, build_sketch_sector_toroidal
+from sector import build_sketch_sector, build_sketch_sector_toroidal, \
+                    get_toroidal_coordinates_tangent
 from geometry_plotter import plot_geometry
+from geometry_operations import rotate_poloidal_section
 
 if __name__ == "__main__":
     poloidal_sections = get_poloidal_sections_from_toroidal_file("./inputs/toroidal_section.json")
     x_collections = []
     y_collections = []
+    x_moved_collections = []
+    y_moved_collections = []
+    z_moved_collections = []
     ctrl_x_collections = []
     ctrl_y_collections = []
-    for poloidal_file in poloidal_sections:
+    toroid_file = get_geometry_parameters_from_toroidal_file("./inputs/toroidal_section.json")
+    (x, y, z), (ctrl_x, ctrl_y, ctrl_z) = build_sketch_sector_toroidal(
+                                                        toroid_file['theta'],
+                                                        toroid_file['phi'],
+                                                        toroid_file['radius'],
+                                                        toroid_file['degree'],
+                                                        toroid_file['weights'])
+    toroidal_coordinates, toroidal_tangents = get_toroidal_coordinates_tangent(
+                                                        toroid_file['sections'],
+                                                        [x, y, z])
+    for i, poloidal_file in enumerate(poloidal_sections):
         poloid_file = get_geometry_parameters_from_poloidal_file("./inputs/" +
                                                     poloidal_file + ".json")
         x, y, ctrl_x, ctrl_y = build_sketch_sector(poloid_file['theta'],
@@ -27,11 +42,13 @@ if __name__ == "__main__":
         y_collections.append(y)
         ctrl_x_collections.append(ctrl_x)
         ctrl_y_collections.append(ctrl_y)
-    toroid_file = get_geometry_parameters_from_toroidal_file("./inputs/toroidal_section.json")
-    (x, y, z), (ctrl_x, ctrl_y, ctrl_z) = build_sketch_sector_toroidal(toroid_file['theta'],
-                                                                   toroid_file['phi'],
-                                                                   toroid_file['radius'],
-                                                                    toroid_file['degree'],
-                                                                    toroid_file['weights'])
+        # moved_points = move_poloidal_section_origin([x, y], toroidal_coordinates[i])
+        moved_points = rotate_poloidal_section([x, y, [0.0]*len(x)],
+                                               toroidal_coordinates[i],
+                                               toroidal_tangents[i])
+        x_moved_collections.append(moved_points[0])
+        y_moved_collections.append(moved_points[1])
+        z_moved_collections.append(moved_points[2])
     plot_geometry([x_collections, y_collections], [ctrl_x_collections, ctrl_y_collections],
-                  [x, y, z], [ctrl_x, ctrl_y, ctrl_z])
+                  [x, y, z], [ctrl_x, ctrl_y, ctrl_z],
+                  [x_moved_collections, y_moved_collections, z_moved_collections])
