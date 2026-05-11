22c8c9008f22ea11f19e354acf27149a8e8bbe83
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2026-05-12 00:23:10 +0200

    elongation computation

--- objectives.py
+++ objectives.py
@@ -3,14 +3,13 @@
 import numpy as np
 
 def compute_elongation(cross_section):
-    """
-    Computes elongatio gicing cross section
-    """
+    """Compute the elongation of a given cross section."""
     z_max = np.max(cross_section[:, 2])
     z_min = np.min(cross_section[:, 2])
-    r_val = np.sqrt(cross_section[:, 0]**2 + cross_section[:, 1]**2)
-    r_max = np.max(r_val)
-    r_min = np.min(r_val)
-    epsilon = (z_max - z_min) / (r_max - r_min)
-
-    return epsilon
+    r = np.sqrt(cross_section[:, 0]**2 + cross_section[:, 1]**2)
+    r_max = np.max(r)
+    r_min = np.min(r)
+    denom = r_max - r_min
+    if denom < 1e-12:
+        return np.inf  # or a large penalty value
+    return (z_max - z_min) / denom
