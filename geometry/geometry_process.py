b76ac4635a6046cf95b17b6ff22260b5278be545
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2026-05-12 01:28:00 +0200

    print function call

--- geometry/geometry_process.py
+++ geometry/geometry_process.py
@@ -1,7 +1,6 @@
 from launch_geometry import geometry_construct, geometry_calculate
 from geometry.geometry_reader import linearize_data, delinearize_data
-from scipy.optimize import minimize
-
+import numpy as np
 
 def geometry_process():
     #poloidal_sections, toroidal_sections = geometry_init()
@@ -12,5 +11,7 @@ def geometry_process():
 def geometry_process_optimization(x0, format):
     toroidal_sections, poloidal_sections = delinearize_data(x0, format)
     elongation = geometry_calculate(toroidal_sections, poloidal_sections)
+    # print("RAW x:", x0[:5])
+    # print("objective:", elongation)
     return elongation
     
