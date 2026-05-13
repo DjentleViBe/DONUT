4e8d00d0f6e2c24bf7684ead91532865153263e5
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2026-05-13 22:32:51 +0200

    INIT

--- geometry/geometry_reader.py
+++ geometry/geometry_reader.py
@@ -6,8 +6,11 @@ in the input directory and read the JSON files to extract the necessary
 parameters for building the sketches of the sectors.
 """
 import json
+from matplotlib.pylab import norm
 import numpy as np
+import config as cfg
 from geometry.geometry_check import param_num_sector, param_num_sector_toroidal
+from helper.fourier_helper import fourier_encode, fourier_decode
 
 def get_poloidal_sections_from_toroidal_file(filename):
     """Read the toroidal section file to get the list of poloidal sections to process.
@@ -77,9 +80,13 @@ def linearize_data(toroidal_file):
     weights = np.array(toroidal_prop.get("weights"))
     sections = np.array(toroidal_prop.get("sections"))
 
-    phi_feat = np.stack([np.sin(phi), np.cos(phi)], axis=-1).flatten()
-    theta_feat = np.stack([np.sin(theta), np.cos(theta)], axis=-1).flatten()
-    combined = np.concatenate([phi_feat, theta_feat, radius, weights, sections])
+    sin_phi_coeffs   = fourier_encode(np.sin(phi), cfg.PHI_T)
+    cos_phi_coeffs   = fourier_encode(np.cos(phi), cfg.PHI_T)
+    
+    sin_theta_coeffs = fourier_encode(np.sin(theta), cfg.THETA_T)
+    cos_theta_coeffs = fourier_encode(np.cos(theta), cfg.THETA_T)
+
+    combined = np.concatenate([sin_phi_coeffs, cos_phi_coeffs, sin_theta_coeffs, cos_theta_coeffs, radius, weights, sections])
     toroidal_array.append(combined)
     # loop through the poloidal files
     for i in range (0, toroidal_prop['N_t']):
@@ -91,8 +98,9 @@ def linearize_data(toroidal_file):
             weights = np.array(data.get("weights"))
             degree = data.get("degree")
 
-            theta_feat = np.stack([np.sin(theta), np.cos(theta)], axis=-1).flatten()
-            combined = np.concatenate([theta_feat, radius, weights])
+            sin_theta_coeffs = fourier_encode(np.sin(theta), cfg.THETA_P)
+            cos_theta_coeffs = fourier_encode(np.cos(theta), cfg.THETA_P)
+            combined = np.concatenate([sin_theta_coeffs, cos_theta_coeffs, radius, weights])
             poloidal_array.append(combined)
             format.append(nval)
     poloidal_flat = np.concatenate(poloidal_array) if poloidal_array else np.array([])
@@ -113,12 +121,15 @@ def delinearize_data(x0, format_list):
     # -----------------------
     N_t = format_list[0]
     poloidal_sections = []
-
+    ncoeff_p = 2 * cfg.THETA_P + 1
     for i in range(N_t):
         N_s = format_list[i + 1]
 
-        theta_feat = x0[idx : idx + 2 * N_s]
-        idx += 2 * N_s
+        sin_theta_coeffs = x0[idx : idx + ncoeff_p]
+        idx += ncoeff_p
+
+        cos_theta_coeffs = x0[idx : idx + ncoeff_p]
+        idx += ncoeff_p
 
         radius = x0[idx : idx + N_s]
         idx += N_s
@@ -126,10 +137,12 @@ def delinearize_data(x0, format_list):
         weights = x0[idx : idx + N_s]
         idx += N_s
 
-        theta_feat = theta_feat.reshape(N_s, 2)
-        sin_theta = theta_feat[:, 0]
-        cos_theta = theta_feat[:, 1]
-        theta = np.degrees(np.arctan2(sin_theta, cos_theta))
+        sin_theta = fourier_decode(sin_theta_coeffs, N_s, cfg.THETA_P)
+        cos_theta = fourier_decode(cos_theta_coeffs, N_s, cfg.THETA_P)
+        norm = np.sqrt(sin_theta**2 + cos_theta**2)
+        sin_theta /= np.maximum(norm, 1e-12)
+        cos_theta /= np.maximum(norm, 1e-12)
+        theta = np.mod(np.degrees(np.arctan2(sin_theta, cos_theta)), 360.0)
 
         poloidal_sections.append({
             "N_s": N_s,
@@ -142,13 +155,19 @@ def delinearize_data(x0, format_list):
     # -----------------------
     # Toroidal reconstruction
     # -----------------------
+    n_coeff_phi_t = 2 * cfg.PHI_T + 1
+    n_coeff_theta_t = 2 * cfg.THETA_T + 1
     N_t = format_list[0]
 
-    phi_feat = x0[idx : idx + 2 * N_t]
-    idx += 2 * N_t
+    sin_phi_coeffs = x0[idx : idx + n_coeff_phi_t]
+    idx += n_coeff_phi_t
+    cos_phi_coeffs = x0[idx : idx + n_coeff_phi_t]
+    idx += n_coeff_phi_t
 
-    theta_feat = x0[idx : idx + 2 * N_t]
-    idx += 2 * N_t
+    sin_theta_coeffs = x0[idx : idx + n_coeff_theta_t]
+    idx += n_coeff_theta_t
+    cos_theta_coeffs = x0[idx : idx + n_coeff_theta_t]
+    idx += n_coeff_theta_t
 
     radius = x0[idx : idx + N_t]
     idx += N_t
@@ -159,11 +178,20 @@ def delinearize_data(x0, format_list):
     sections = x0[idx : idx + N_t]
     idx += N_t
 
-    phi_feat = phi_feat.reshape(N_t, 2)
-    theta_feat = theta_feat.reshape(N_t, 2)
+    sin_phi = fourier_decode(sin_phi_coeffs, N_t, cfg.PHI_T)
+    cos_phi = fourier_decode(cos_phi_coeffs, N_t, cfg.PHI_T)
+    norm = np.sqrt(sin_phi**2 + cos_phi**2)
+    sin_phi /= np.maximum(norm, 1e-12)
+    cos_phi /= np.maximum(norm, 1e-12)
 
-    phi = np.degrees(np.arctan2(phi_feat[:, 0], phi_feat[:, 1]))
-    theta = np.degrees(np.arctan2(theta_feat[:, 0], theta_feat[:, 1]))
+    sin_theta = fourier_decode(sin_theta_coeffs, N_t, cfg.THETA_T)
+    cos_theta = fourier_decode(cos_theta_coeffs, N_t, cfg.THETA_T)
+    norm = np.sqrt(sin_theta**2 + cos_theta**2)
+    sin_theta /= np.maximum(norm, 1e-12)
+    cos_theta /= np.maximum(norm, 1e-12)
+
+    phi = np.mod(np.degrees(np.arctan2(sin_phi, cos_phi)), 360.0)
+    theta = np.mod(np.degrees(np.arctan2(sin_theta, cos_theta)), 360.0)
 
     toroidal_sections = {
         "N_t": N_t,
@@ -174,5 +202,4 @@ def delinearize_data(x0, format_list):
         "degree": 3,
         "sections": sections
     }
-
     return toroidal_sections, poloidal_sections
--- launch_geometry.py
+++ launch_geometry.py
@@ -7,6 +7,7 @@ The module also computes the elongation of the guide vanes
 and plots the geometry for verification.
 """
 import numpy as np
+from scipy.special import logsumexp
 import config as cfg
 import geometry.geometry_process as gp
 from geometry.geometry_reader import get_poloidal_sections_from_toroidal_file, \
@@ -110,7 +111,7 @@ def geometry_construct(toroidal_sections, poloidal_sections, mode, plot=False, f
             file_list.append(f"./outputs/revolved_surface+{i}.stl")
         vals = [compute_elongation(np.asarray(guide_vane_collections[i])[:, j, :])
                 for j in range(100)]
-        epsilon_max = np.log(np.sum(np.exp(cfg.K_SMOOTH * np.array(vals)))) / cfg.K_SMOOTH
+        epsilon_max = logsumexp(cfg.K_SMOOTH * np.array(vals)) / cfg.K_SMOOTH
         elongation_list.append(epsilon_max)
     gp.CURRENT_ELONGATION = np.percentile(elongation_list, 95)
     if plot:
@@ -185,7 +186,7 @@ def geometry_calculate(toroidal_sections, poloidal_sections):
         )
         vals = [compute_elongation(np.asarray(guide_vane_collections[i])[:, j, :])
                         for j in range(100)]
-        epsilon_max = np.log(np.sum(np.exp(cfg.K_SMOOTH * np.array(vals)))) / cfg.K_SMOOTH
+        epsilon_max = logsumexp(cfg.K_SMOOTH * np.array(vals)) / cfg.K_SMOOTH
         elongation_list.append(epsilon_max)
     gp.CURRENT_ELONGATION = np.percentile(elongation_list, 95)
     return gp.CURRENT_ELONGATION
