56b41fa5e7d92cdc0743b19fb38600cd5fbbe45d
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2026-05-16 07:49:38 +0200

    ar constraint params

--- config.py
+++ config.py
@@ -20,6 +20,9 @@ THETA_P = 2
 DELTA_MIN = 0.0
 DELTA_MAX = 0.25
 PENALTY_WEIGHT = 1000.0
+# Aspect ratio constraint parameters
+AR_MIN = 2.0
+AR_MAX = 10.0
 # plotting parameters
 color = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728",
           "#9467bd", "#8c564b", "#e377c2", "#7f7f7f",
