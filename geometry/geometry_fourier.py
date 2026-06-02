d75a4bd9387c188687e00823266377a597ab09a9
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2026-06-02 23:41:40 +0200

    INIT : type 3 logic for geometry load

--- geometry/geometry_fourier.py
+++ geometry/geometry_fourier.py
@@ -27,7 +27,7 @@ def linearize_data(toroidal_file):
     weights = np.array(toroidal_prop.get("weights"), dtype=np.float64)
     phi_to_u = f_to_u(phi)
 
-    if cfg.STUDY_NAME == "Type1":
+    if cfg.STUDY_NAME == "Type1" or cfg.STUDY_NAME == "Type3":
         sections = np.array(toroidal_prop.get("sections"), dtype=np.float64)
         combined = np.concatenate([phi_to_u, theta, radius, weights, sections])
     elif cfg.STUDY_NAME == "Type2":
@@ -41,20 +41,28 @@ def linearize_data(toroidal_file):
     for i in range (0, toroidal_prop['N_t']):
         if cfg.STUDY_NAME == "Type1":
             filename = "./inputs/poloidal_section_" + str(i + 1) + ".json"
+            with open(filename, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                psi = np.array(data.get("psi")) * np.pi / 180.0
+            psi = f_to_u(psi)
         elif cfg.STUDY_NAME == "Type2":
             filename = "./inputs/poloidal_section.json"
-        with open(filename, "r", encoding="utf-8") as f:
-            data = json.load(f)
-            nval = data.get("N_s")
-            psi = np.array(data.get("psi")) * np.pi / 180.0
-            radius = np.array(data.get("radius"), dtype=np.float64)
-            weights = np.array(data.get("weights"), dtype=np.float64)
-            degree = data.get("degree")
-
-            psi_to_u = f_to_u(psi)
-            combined = np.concatenate([psi_to_u, radius, weights])
-            poloidal_array.append(combined)
-            format.append(nval)
+            with open(filename, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                psi = np.array(data.get("psi")) * np.pi / 180.0
+            psi = f_to_u(psi)     
+        elif cfg.STUDY_NAME == "Type3":
+            filename = "./inputs/poloidal_section_" + str(i + 1) + ".json"
+            with open(filename, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                psi = np.array(data.get("psi")) * np.pi / 180.0
+        nval = data.get("N_s")  
+        radius = np.array(data.get("radius"), dtype=np.float64)
+        weights = np.array(data.get("weights"), dtype=np.float64)
+        degree = data.get("degree")
+        combined = np.concatenate([psi, radius, weights])
+        poloidal_array.append(combined)
+        format.append(nval)
         if cfg.STUDY_NAME == "Type2":
             break
     poloidal_flat = np.concatenate(poloidal_array) if poloidal_array else np.array([])
@@ -123,6 +131,29 @@ def delinearize_data(x0):
                 "weights": weights,
                 "degree": 3
             })
+    elif cfg.STUDY_NAME == "Type3":
+        N_t = format_list[0]
+        for i in range(N_t):
+            N_s = format_list[i + 1]
+
+            psi_to_u = x0[idx : idx + N_s]
+            idx += N_s
+
+            radius = x0[idx : idx + N_s]
+            idx += N_s
+
+            weights = x0[idx : idx + N_s]
+            idx += N_s
+
+            psi = np.degrees(psi_to_u)
+
+            poloidal_sections.append({
+                "N_s": N_s,
+                "psi": psi,
+                "radius": radius,
+                "weights": weights,
+                "degree": 3
+            })
     
     # -----------------------
     # Toroidal reconstruction
@@ -144,7 +175,7 @@ def delinearize_data(x0):
     phi = np.degrees(u_to_f(phi_to_u, 0))
     theta = np.degrees(theta0 + theta_to_x * theta_delta)  # direct decoding for theta
     
-    if cfg.STUDY_NAME == "Type1":
+    if cfg.STUDY_NAME == "Type1" or cfg.STUDY_NAME == "Type3":
         sections = x0[idx : idx + N_t]
         toroidal_sections = {
         "N_t": N_t,
@@ -178,5 +209,5 @@ def delinearize_data(x0):
         "A" : A,
         "k" : k
         }
-
+    
     return toroidal_sections, poloidal_sections
